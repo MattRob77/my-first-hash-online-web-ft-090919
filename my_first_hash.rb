81211ce242da7518b8d77ed9fbd9b0e931c03763
Matthew Robinson <matt_robinson1@yahoo.com>
2019-09-11 21:17:30 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -25,7 +25,7 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-  shipping_manifest[muskets]
+  shipping_manifest["muskets"]
 
 
 
